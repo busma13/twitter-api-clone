4fe337e197add6b5f7ba2e3d122efaa92e0ae64a
busma13 <peterluitjens@gmail.com>
2023-03-28 07:42:53 -0700

    Create ErrorDto and ControllerAdvice classes.

---
+++ assessment1Team3/src/main/java/com/cooksys/assessment1Team3/controllers/advice/Assessment1Team3ControllerAdvice.java
@@ -0,0 +1,37 @@
+package com.cooksys.assessment1Team3.controllers.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.cooksys.assessment1Team3.dtos.ErrorDto;
+import com.cooksys.assessment1Team3.exceptions.BadRequestException;
+import com.cooksys.assessment1Team3.exceptions.NotAuthorizedException;
+import com.cooksys.assessment1Team3.exceptions.NotFoundException;
+
+@ControllerAdvice(basePackages = { "com.cooksys.assessment1Team3.controllers"})
+@ResponseBody
+public class Assessment1Team3ControllerAdvice {
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadRequestException.class)
+	public ErrorDto handleBadRequestException(BadRequestException badRequestException) {
+		return new ErrorDto(badRequestException.getMessage());
+	}
+	
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(NotAuthorizedException.class)
+	public ErrorDto handleNotAuthorizedException(NotAuthorizedException notAuthorizedException) {
+		return new ErrorDto(notAuthorizedException.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotFoundException(NotFoundException notFoundException) {
+		return new ErrorDto(notFoundException.getMessage());
+	}
+
+	
+}
